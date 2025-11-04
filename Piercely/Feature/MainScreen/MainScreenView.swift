3134c29bd3f05c8784725e514b5f3efa6c1527f7
Grigory Shilyaev <g.shilyaev28@gmail.com>
2025-11-04 14:28:55 +0700

    main: mock of the main screen

---
+++ Piercely/Feature/MainScreen/MainScreenView.swift
@@ -0,0 +1,19 @@
+//
+//  MainScreenView.swift
+//  Piercely
+//
+//  Created by Grigorii Shiliaev on 04.11.25.
+//
+
+import SwiftUI
+
+struct MainAppView: View {
+    var body: some View {
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            Text("Main App")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+        }
+    }
+}
