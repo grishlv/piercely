4a24d71a2b8912c9eecd371666d437797739722e
Grigory Shilyaev <g.shilyaev28@gmail.com>
2025-11-04 14:28:38 +0700

    main: Factory of Onboarding

---
+++ Piercely/Feature/Onboarding/Factory/OnboardingScreenBuilder.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingScreenBuilder.swift
+//  Piercely
+//
+//  Created by Grigorii Shiliaev on 04.11.25.
+//
+
+import SwiftUI
+
+final class OnboardingScreenBuilder {
+    // MARK: Private properties
+    private let screenFactory: OnboardingScreenFactory
+    
+    // MARK: Life cycle
+    init(screenFactory: OnboardingScreenFactory) {
+        self.screenFactory = screenFactory
+    }
+}
+
+// MARK: - Public methods
+extension OnboardingScreenBuilder {
+    func buildView() -> any View {
+        let viewModel = screenFactory.makeOnboardingScreenViewModel()
+
+        let view = OnboardingScreenView(viewModel: viewModel)
+        
+        return view
+    }
+}
---
+++ Piercely/Feature/Onboarding/Factory/OnboardingScreenFactory.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingScreenFactory.swift
+//  Piercely
+//
+//  Created by Grigorii Shiliaev on 04.11.25.
+//
+
+import Foundation
+
+final class OnboardingScreenFactory {
+}
+
+extension OnboardingScreenFactory {
+    func makeOnboardingScreenViewModel() -> some OnboardingScreenViewModelImpl {
+        OnboardingScreenViewModelImpl()
+    }
+}
