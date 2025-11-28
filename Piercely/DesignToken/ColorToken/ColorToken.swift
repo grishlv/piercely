4a6b59606bdbe01157ceacbd7590c3a9831d6221
Grigory Shilyaev <g.shilyaev28@gmail.com>
2025-11-28 20:51:19 +0700

    main: Working on assets on the onboarding screen

---
+++ Piercely/DesignToken/ColorToken/ColorToken.swift
@@ -0,0 +1,15 @@
+//
+//  ColorToken.swift
+//  Piercely
+//
+//  Created by Grigorii Shiliaev on 28.11.25.
+//
+
+import SwiftUI
+
+enum ColorToken {
+    static let primaryBackground = Color(hex: "#1F2125")
+    static let whiteColor = Color(hex: "#FFFFFF")
+    static let subtitleColor = Color(hex: "#A2A2A2")
+    static let blueButtonColor = Color(hex: "#3072DF")
+}
---
+++ Piercely/DesignToken/FontToken/FontToken.swift
@@ -0,0 +1,15 @@
+//
+//  FontToken.swift
+//  Piercely
+//
+//  Created by Grigorii Shiliaev on 28.11.25.
+//
+
+import SwiftUI
+
+enum FontToken {
+    static let headline = Font.custom("SF-Pro", size: 64)
+    static let headlineAccent = Font.custom("ShadowsIntoLight", size: 56)
+    static let subtitle = Font.custom("SF-Pro", size: 22)
+    static let buttonOnboardingTitle = Font.custom("SF-Pro", size: 22)
+}
