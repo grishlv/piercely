f94eed844e6f37d2692a39359e7d12299a62fb0f
Grigory Shilyaev <g.shilyaev28@gmail.com>
2025-12-02 11:50:44 +0700

    main: Fixed fonts and changed assets

--- Piercely/DesignToken/FontToken/FontToken.swift
+++ Piercely/DesignToken/FontToken/FontToken.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 enum FontToken {
-    static let headline = Font.custom("SF-Pro", size: 64)
+    static let headline = Font.custom("SFProText-Regular", size: 64)
     static let headlineAccent = Font.custom("ShadowsIntoLight", size: 56)
-    static let subtitle = Font.custom("SF-Pro", size: 22)
-    static let buttonOnboardingTitle = Font.custom("SF-Pro", size: 22)
-    static let buttonMainScreenTitle = Font.custom("SF-Pro", size: 22)
+    static let subtitle = Font.custom("SFProText-Regular", size: 22)
+    static let titleMainScreen = Font.custom("SFProText-Medium", size: 20)
+    static let buttonOnboardingTitle = Font.custom("SFProText-Regular", size: 22)
+    static let buttonMainScreenTitle = Font.custom("SFProText-Bold", size: 12)
 }
